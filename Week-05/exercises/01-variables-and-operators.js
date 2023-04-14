a67855d0bed1ec0c01b0b396649bc6380eb8c16d
lupomartin <lupomartin2003@gmail.com>
2023-04-14 14:04:29 -0300

    BaSP-W05-2023: complete variables and operators

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,18 @@
+//Ejercicio 1
+//Ejercicio 1.A
+var a = 10;
+var b = 5;
+var resultado = a + b;
+console.log('Ejercicio 1.A: ' + resultado);
+
+//Ejercicio 1.B
+var nombre = 'Martin';
+var nacionalidad = 'Argentina';
+var concatenar = 'Mi nombre es ' + nombre + ' y naci en ' + nacionalidad;
+console.log('Ejercicio 1.B: ' + concatenar);
+
+//Ejercicio 1.C
+var pais = 'EstadosUnidos';
+var estado = 'Florida';
+var length = pais + estado;
+console.log('Ejercicio 1.C: ' + length .length);
