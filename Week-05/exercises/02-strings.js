cc917e9359f04af6d7e4780728e74c768f360404
lupomartin <lupomartin2003@gmail.com>
2023-04-14 15:27:36 -0300

    BaSP-W05-2023: complete strings

--- Week-05/exercises/02-strings.js
+++ Week-05/exercises/02-strings.js
@@ -7,11 +7,28 @@ console.log('Ejercicio 2.A: ' + mayusculas);
 
 //Ejercicio 2.B
 var pais;
-var subpais = pais.substring(0,5);
-console.log('Ejercicio 2.B: ' +subpais);
+var subpais = pais.substring(0, 5);
+console.log('Ejercicio 2.B: ' + subpais);
 
-//Ejercicios 3.C
+//Ejercicio 2.C
 
 var pais;
-var paisub = pais.substring(pais.length -3);
-console.log('Ejercicio 2.C: ' +paisub);
+var paisub = pais.substring(pais.length - 3);
+console.log('Ejercicio 2.C: ' + paisub);
+
+//Ejercicio 2.D
+var pais;
+var mayusminus = pais.substring(0, 1).toUpperCase() + pais.substring(1).toLowerCase();
+console.log('Ejercicio 2.D: ' + mayusminus);
+
+//Ejercicio 2.E
+var espacio = 'soy martin';
+var espacioBlanco = espacio.indexOf(' ');
+console.log('Ejercicio 2.E: ' + espacioBlanco);
+
+//Ejercicio 2.F
+var palabraLarga = 'computadora microondas';
+var computadora = palabraLarga.substring(0, palabraLarga.indexOf(' '));
+var microondas = palabraLarga.substring(palabraLarga.indexOf(' ') + 1);
+let junte = computadora[0].toUpperCase() + computadora.substring(1).toLowerCase() + " " + microondas[0].toUpperCase() + microondas.substring(1).toLowerCase();
+console.log('Ejercicio 2.F: ' +junte);
